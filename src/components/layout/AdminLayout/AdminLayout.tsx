7f41fd43b72c7542cda11bb8d6e2a3a5a9683a44
David Allenby <hello@davidallenby.com>
2024-07-26 08:01:27 +0200

    Moving files

--- src/components/layout/AdminLayout/AdminLayout.tsx
+++ src/components/layout/AdminLayout/AdminLayout.tsx
@@ -1,6 +1,6 @@
 import { FC, ReactNode } from "react";
 import AdminSidebar from "@components/ui/AdminSidebar/AdminSidebar";
-import './AdminLayoutContainer.scss';
+import './AdminLayout.scss';
 
 interface AdminLayoutProps {
   children: ReactNode;
