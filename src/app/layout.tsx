8fbefcc8b2775a94f714ed6c2dca5d36ea9356db
David Allenby <hello@davidallenby.com>
2024-09-23 22:52:16 +0200

    Minor content changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,12 @@
-import type { Metadata } from "next";
+import type { Metadata } from 'next';
 import '../styles/index.scss';
-import '../../node_modules/react-loading-skeleton/dist/skeleton.css'
-import ReactQueryProvider from "@context/ReactQueryProvider"
+import '../../node_modules/react-loading-skeleton/dist/skeleton.css';
+import ReactQueryProvider from '@context/ReactQueryProvider';
 
 export const metadata: Metadata = {
-  title: `David Allenby | Lead frontend developer based in Berlin, Germany`,
-  description: "Hi, I'm David. I'm a lead frontend developer based in Berlin, Germany. I like making apps, and taking naps.",
+  title: `David Allenby | Lead frontend developer based in Melbourne, Australia`,
+  description:
+    "Hi, I'm David. I'm a lead frontend developer based in Melbourne, Australia. I like making apps, and taking naps.",
 };
 
 export default function RootLayout({
@@ -13,18 +14,13 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-
   return (
-    <html lang="en" className="d-flex flex-column flex-grow-1 h-100">
+    <html lang='en' className='d-flex flex-column flex-grow-1 h-100'>
       <head>
-      <script>
-        let FF_FOUC_FIX;
-      </script>
+        <script>let FF_FOUC_FIX;</script>
       </head>
-      <body className="d-flex flex-column flex-grow-1">
-        <ReactQueryProvider>
-          { children }
-        </ReactQueryProvider>
+      <body className='d-flex flex-column flex-grow-1'>
+        <ReactQueryProvider>{children}</ReactQueryProvider>
       </body>
     </html>
   );
--- src/components/ui/SiteHeader/SiteHeader.tsx
+++ src/components/ui/SiteHeader/SiteHeader.tsx
@@ -1,9 +1,8 @@
-'use client'
+'use client';
 
-import React, { FC, useEffect, useState } from 'react';
+import React, { FC } from 'react';
 import './SiteHeader.scss';
 import SiteLogo from '@components/ui/SiteLogo/SiteLogo';
-import { debounce } from '../../../helpers/common';
 import ToggleMobileNav from '../ToggleMobileNav/ToggleMobileNav';
 import SiteNav from '../SiteNav/SiteNav';
 import MobileMenu from '../MobileMenu/MobileMenu';
@@ -15,21 +14,18 @@ interface SiteHeaderProps {}
 const SiteHeader: FC<SiteHeaderProps> = () => {
   const { isMobile } = useBreakpointBoolean();
 
-
   return (
     <MobileNavContextProvider>
-      <div className="SiteHeader">
-        <div className="contained position-relative d-flex align-items-center justify-content-between gutter-x py-3">
-          <SiteLogo 
-            className='me-4'
-          />
+      <header className='SiteHeader'>
+        <div className='contained position-relative d-flex align-items-center justify-content-between gutter-x py-3'>
+          <SiteLogo className='me-4' />
           <ToggleMobileNav className='d-md-none' />
           <SiteNav className='d-none d-md-inline-flex' />
-          { isMobile && <MobileMenu />}
+          {isMobile && <MobileMenu />}
         </div>
-      </div>
+      </header>
     </MobileNavContextProvider>
   );
-}
+};
 
 export default SiteHeader;
