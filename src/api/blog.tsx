8f109c7c13361929a2cd702954b1805528db2449
David Allenby <hello@davidallenby.com>
2025-06-28 19:08:39 +1000

    Brought down cache limit

--- src/api/blog.tsx
+++ src/api/blog.tsx
@@ -1,36 +1,42 @@
-import { CMS } from "@constants/wordpress";
-import { ApiResponse } from "@interfaces/api.interfaces";
-import { 
-  GetBlogPostsPayload, BlogPost, 
+import { CMS } from '@constants/wordpress'
+import type { ApiResponse } from '@interfaces/api.interfaces'
+import type {
+  BlogPost,
   BlogPostCategory,
-  BlogPostTag
-} from "@interfaces/blog.interfaces";
-import { WPBlogPost, WpCategory, WpTag } from "@interfaces/wordpress.interfaces";
+  BlogPostTag,
+  GetBlogPostsPayload
+} from '@interfaces/blog.interfaces'
+import type {
+  WPBlogPost,
+  WpCategory,
+  WpTag
+} from '@interfaces/wordpress.interfaces'
 
 /**
  * Get a list of blog posts
- * @param payload 
- * @returns 
+ * @param payload
+ * @returns
  */
-export const getBlogPosts = async (payload: GetBlogPostsPayload)
-: Promise<BlogPost[]> => {
+export const getBlogPosts = async (
+  payload: GetBlogPostsPayload
+): Promise<BlogPost[]> => {
   try {
-    const { page, tagIds } = payload;
-    let url = `${CMS.BASE_URL}/posts?page=${page}`;
+    const { page, tagIds } = payload
+    let url = `${CMS.BASE_URL}/posts?page=${page}`
 
     if (tagIds?.length) {
-      tagIds.forEach((id) => {
+      for (const id of tagIds) {
         url += `&tags=${id}`
-      })
+      }
     }
 
-    const data = await fetch(url);
-    const json = await data.json();
+    const data = await fetch(url)
+    const json = await data.json()
     // Re-map the WP items as blog post items for the client side / NextJS
-    return json.map((item: WPBlogPost) => setWpBlogPostAsBlogPost(item));
+    return json.map((item: WPBlogPost) => setWpBlogPostAsBlogPost(item))
   } catch (err) {
-    console.log(err);
-    return [];
+    console.log(err)
+    return []
   }
 }
 // Converts the WP response item to a better format for our frontend. There are
@@ -54,54 +60,55 @@ const setWpBlogPostAsBlogPost = (item: WPBlogPost): BlogPost => {
 /**
  * Get a list of categories for blog posts
  */
-export const getBlogPostCategories = async(): Promise<BlogPostCategory[]> => {
+export const getBlogPostCategories = async (): Promise<BlogPostCategory[]> => {
   try {
     const url = `${CMS.BASE_URL}/categories?hide_empty=true`
-    const data = await fetch(url);
-    const json = await data.json();
+    const data = await fetch(url)
+    const json = await data.json()
     // Re-map the WP items as blog post items for the client side / NextJS
-    return json.map(mapWpItemsAsClient);
+    return json.map(mapWpItemsAsClient)
   } catch (err) {
-    console.log(err);
-    return [];
+    console.log(err)
+    return []
   }
 }
-
 
 /**
  * Get a list of tags
- * @returns 
+ * @returns
  */
-export const getBlogPostTags = async(): Promise<BlogPostTag[]> => {
+export const getBlogPostTags = async (): Promise<BlogPostTag[]> => {
   try {
     const url = `${CMS.BASE_URL}/tags`
-    const data = await fetch(url);
-    const json = await data.json();
+    const data = await fetch(url)
+    const json = await data.json()
     // Re-map the WP items as blog post tags for the client side / NextJS
-    return json.map(mapWpItemsAsClient);
+    return json.map(mapWpItemsAsClient)
   } catch (err) {
-    console.log(err);
-    return [];
+    console.log(err)
+    return []
   }
 }
 // Categories and Tags have the same structure
-type WpTagOrWpCategory = WpTag | WpCategory;
-const mapWpItemsAsClient = (item: WpTagOrWpCategory)
-: BlogPostTag|BlogPostCategory => {
-  const { id, name, slug } = item;
-  return { id, name, slug };
+type WpTagOrWpCategory = WpTag | WpCategory
+const mapWpItemsAsClient = (
+  item: WpTagOrWpCategory
+): BlogPostTag | BlogPostCategory => {
+  const { id, name, slug } = item
+  return { id, name, slug }
 }
 /**
  * Get a specific blog post by slug
- * @param slug 
- * @returns 
+ * @param slug
+ * @returns
  */
-export const getBlogPostBySlug = async (slug: string)
-: ApiResponse<BlogPost> => {
+export const getBlogPostBySlug = async (
+  slug: string
+): ApiResponse<BlogPost> => {
   try {
     const url = `${CMS.BASE_URL}/posts?slug=${slug}`
-    const data = await fetch(url, { next: { revalidate: 3600 }});
-    const json = await data.json();
+    const data = await fetch(url, { next: { revalidate: 600 } })
+    const json = await data.json()
     const mapped = json.map((item: WPBlogPost) => setWpBlogPostAsBlogPost(item))
     return { data: mapped[0], success: true }
   } catch {
