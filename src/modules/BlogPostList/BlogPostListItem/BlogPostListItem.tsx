99bcfe0eba8bf6cf0cdf361f2da4d05c679d08bf
David Allenby <hello@davidallenby.com>
2025-11-15 17:07:19 +1100

    Fix loading skeleton layout on blog list page

--- src/modules/BlogPostList/BlogPostListItem/BlogPostListItem.tsx
+++ src/modules/BlogPostList/BlogPostListItem/BlogPostListItem.tsx
@@ -1,46 +1,35 @@
-import SanitizedHtml from '@components/ui/SanitizedHtml/SanitizedHtml'
 import { getDateString } from '@helpers/dates'
 import type { BlogPost } from '@interfaces/blog.interfaces'
 import BlogPostListItemImage from '@modules/BlogPostList/BlogPostListItem/BlogPostListItemImage'
 import { type FC, useMemo } from 'react'
 import { BlogPostListItemBase } from './BlogPostListItemBase'
+import { BlogPostListItemContent } from './BlogPostListItemContent'
 import { BlogPostListItemProvider } from './BlogPostListItemContext'
-import { BlogPostListItemTags } from './BlogPostListItemTags'
 import { getLastFullStopAfterIndex } from './utils'
 
 interface BlogPostListItemProps {
-  post: BlogPost
-  className?: string
+	post: BlogPost
+	className?: string
 }
 
 const BlogPostListItem: FC<BlogPostListItemProps> = ({ post, className }) => {
-  const excerpt = useMemo(() => {
-    return getLastFullStopAfterIndex(post.excerpt)
-  }, [post.excerpt])
+	const excerpt = useMemo(() => {
+		return getLastFullStopAfterIndex(post.excerpt)
+	}, [post.excerpt])
 
-  const dateString = useMemo(() => {
-    return getDateString(post.dateCreated)
-  }, [post.dateCreated])
+	const dateString = useMemo(() => {
+		return getDateString(post.dateCreated)
+	}, [post.dateCreated])
 
-  return (
-    <BlogPostListItemProvider post={post}>
-      <BlogPostListItemBase href={post.url} className={className}>
-        <BlogPostListItemImage
-          src={post.featuredImageUrl}
-          alt={post.title}
-          className='mb-4'
-        />
-        <div>
-          <h3 className='mb-1!'>{post.title}</h3>
-          <p className='subtitle mb-2'>{dateString}</p>
+	return (
+		<BlogPostListItemProvider post={post}>
+			<BlogPostListItemBase href={post.url} className={className}>
+				<BlogPostListItemImage src={post.featuredImageUrl} alt={post.title} />
 
-          <SanitizedHtml html={excerpt} className='mb-4' />
-
-          <BlogPostListItemTags />
-        </div>
-      </BlogPostListItemBase>
-    </BlogPostListItemProvider>
-  )
+				<BlogPostListItemContent title={post.title} dateString={dateString} excerpt={excerpt} />
+			</BlogPostListItemBase>
+		</BlogPostListItemProvider>
+	)
 }
 
 export default BlogPostListItem
--- src/modules/BlogPostList/config.ts
+++ src/modules/BlogPostList/config.ts
@@ -1,2 +1,7 @@
 export const BLOG_POST_LIST_ITEM_BASE_CLASS =
-	'flex flex-col md:flex-row-reverse lg:flex-col xl:flex-row-reverse md:gap-8'
+	'flex flex-col md:flex-row-reverse lg:flex-col xl:flex-row-reverse md:gap-8 gap-4'
+
+export const BLOG_POST_LIST_ITEM_IMAGE_BASE_CLASS =
+	'aspect-video md:aspect-square lg:aspect-video xl:aspect-square block relative w-full md:max-w-[28%] lg:max-w-none xl:max-w-[30%] overflow-hidden'
+
+export const BLOG_POST_LIST_ITEM_CONTENT_BASE_CLASS = 'flex flex-col gap-2 w-full'
